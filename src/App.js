531211cea27fd576108f63b530e89247449f6ca6
Alex Kelly <afkelly14@gmail.com>
2021-09-10 10:51:47 -0400

    react code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Link } from 'react';
+import axios from 'axios';
+import Button from './button';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// a83e52f2ee6d464dbd6e8b3e9b9b263e
 
-export default App;
+function IDme() {  
+  // const accessToken = window.location.hash.split("&")[0].match(/[^#access_token=]\w+/)[0];
+  // const tokenEndpoint = `https://api.id.me/api/public/v3/attributes.json?access_token=${accessToken}`
+
+  // const asyncFetch = async (endpoint) => {
+  //   // console.log(endpoint);
+  //   try {
+  //     const response = await axios(endpoint)
+  //     const data = await response.data
+  //     console.log(data)
+  //     console.log(data.attributes[1].value)
+  //     console.log(data.status[0].verified)
+  //   } catch (error) {
+  //     console.log("Error:", error)
+  //   }
+  // }
+
+   
+  // asyncFetch(tokenEndpoint);
+
+  
+ 
+  return(
+
+        <div>
+  
+          <Button />
+
+        </div>
+   
+  
+
+
+    )//return
+
+}//component
+
+
+
+
+
+export default IDme;
