e11c47aa24c1696248c0148dcf8ba40f97694684
Alex Kelly <afkelly14@gmail.com>
2021-10-25 13:56:46 -0400

    adding error messages

--- src/button.js
+++ src/button.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const errorCodes = {
-  'access_denied': 'You denied consent',
+  'access_denied': 'You have denied the consent to release data to the partner. Please verify again.',
+  'invalid_request':'The owner of this application is missing a required parameter or it is malformed',
+  'invalid_client': 'The owner of this application is using an incorrect Client ID',
+  'invalid_redirect_uri': 'Your Redirect URI is incorrect. Please check the values match',
+  'unsupported_response_type':'The owner of this application has an unsupported response type. Please check the parameters',
+  'invalid_scope': 'The owner of this application has an invalid scope. Please check your policies are enabled and the scope has been updated.'
 }
 
 let error
@@ -18,10 +23,11 @@ if(location) {
 function Button() {
      
   return (
-
+  
     <div className="idmebutton">
-       
+      <div className='errorMessage'>
       {location && displayMessage}
+      </div>
       <a href="https://groups.id.me/?client_id=8ffeb47b23d72aeb90ac446ce7064108&redirect_uri=http://localhost:3000&response_type=token&scopes=military,responder,nurse,hospital_employee">
         <img src="https://developers.id.me/assets/buttons/verify-32b84e457998bb752606ed89415338ec7aec19954ee1d3d4bfa002dd307ad433.svg" />
       </a>
