4b5d18d05ce99db4509aaebd178ea86a55ec36a2
Alex Kelly <afkelly14@gmail.com>
2021-10-28 15:09:57 -0400

    adding vaiables

--- src/button.js
+++ src/button.js
@@ -4,11 +4,17 @@ const errorCodes = {
   'access_denied': 'You have denied the consent to release data to the partner. Please verify again.',
   'invalid_request':'The owner of this application is missing a required parameter or it is malformed',
   'invalid_client': 'The owner of this application is using an incorrect Client ID',
-  'invalid_redirect_uri': 'Your Redirect URI is incorrect. Please check the values match',
+  'invalid_redirect_uri': 'The owner of this application has an Redirect URI is incorrect. Please check the values match',
   'unsupported_response_type':'The owner of this application has an unsupported response type. Please check the parameters',
   'invalid_scope': 'The owner of this application has an invalid scope. Please check your policies are enabled and the scope has been updated.'
 }
 
+
+
+let clientID = 'Insert Client ID here'
+let redirectURI = 'Insert Redirect URI here'
+let scopes = 'Insert scopes here'
+let endpoint = `https://groups.id.me/?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scopes=${scopes}`
 let error
 let displayMessage
 let location = window.location.hash !== ''
@@ -16,7 +22,7 @@ let location = window.location.hash !== ''
 if(location) {
   error = window.location.hash.split("&")[0].match(/[^#error=]\w+/)[0]
   displayMessage = (
-    <span className= 'errorMessage'>{errorCodes[error]}</span>
+    <span className= 'errorMessage'>ERROR: {errorCodes[error]}</span>
   )
 }
 
@@ -26,9 +32,9 @@ function Button() {
   
     <div className="idmebutton">
       <div className='errorMessage'>
-      <p><strong>ERROR:</strong> {location && displayMessage} </p>
+      <p>{location && displayMessage} </p>
       </div>
-      <a href="https://groups.id.me/?client_id=8ffeb47b23d72aeb90ac446ce7064108&redirect_uri=http://localhost:3000&response_type=token&scopes=military,responder,nurse,hospital_employee">
+      <a href={endpoint}>
         <img src="https://developers.id.me/assets/buttons/verify-32b84e457998bb752606ed89415338ec7aec19954ee1d3d4bfa002dd307ad433.svg" />
       </a>
     
